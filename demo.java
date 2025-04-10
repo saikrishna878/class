8274c743711949d7856ddf452bce20b9374cc68f
saikrishna878 <nanaboinasaikrishna555@gmail.com>
2025-04-10 09:55:08 +0530

    Update demo.java

--- demo.java
+++ demo.java
@@ -1,6 +1,4 @@
 public class nitin{
       public static void main(String args[]){
-      System.out.println("Hey F NITIN");
-      System.out.println("Hey check NITIN");
-      }
+      System.out.println("Hey F NITIN");      }
       }
